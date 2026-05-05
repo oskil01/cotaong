80d127c63b45a217454ebdd660ce9211c2555e51
oskil01 <oskill@outlook.fr>
2026-05-05 19:27:18 +0200

    MAJ

--- src/data/resourcesData.js
+++ src/data/resourcesData.js
@@ -1,22 +1,22 @@
 export const resourcesData = {
   techniques: [
-    { title: "Document directeur", file: "/docs/COTA_Document_Directeur_2022.pdf" },
-    { title: "Manuel des procédures Adm&Fin", file: "/docs/COTA_manuel_procedures.pdf" },
-    { title: "Guide de suivi-évaluation des projets", file: "/docs/COTA-Guide_de_suivi-evaluation-2025.pdf" },
-    { title: "Code de conduite PEAS", file: "/docs/COTA_code_de_conduite_PEAS.pdf" },
+    { title: "Document directeur", file: "/docs/COTA_Document_Directeur_2022.pdf"},
+    { title: "Manuel des procédures Adm&Fin", file: "/docs/COTA_manuel_procedures.pdf"},
+    { title: "Guide de suivi-évaluation des projets", file: "/docs/COTA-Guide_de_suivi-evaluation-2025.pdf"},
+    { title: "Code de conduite PEAS", file: "/docs/COTA_code_de_conduite_PEAS.pdf"},
     { title: "Formulaire de Déclaration d'impartialité et Confidentialité", file: "/docs/COTA_Formulaire_de_déclaration_de_conflit_d'intérêts.pdf" },
     { title: "Organigramme", file: "/docs/COTA_Organigramme.pdf" },
-    { title: "Organigramme de programmes", file: "/docs/COTA_ORGANIGRAMME_DE_PROGRAMMES.pdf" },
-    { title: "Matrice de réddition de compte", file: "/docs/COTA_Matrice_de_reddition_de_compte_2026.pdf" },
+    { title: "Organigramme de programmes", file: "/docs/COTA_ORGANIGRAMME_DE_PROGRAMMES.pdf"},
+    { title: "Matrice de réddition de compte", file: "/docs/COTA_Matrice_de_reddition_de_compte_2026.pdf"},
   ],
   rapports: [
-    { title: "Rapport de consultance : Evaluation finale externe du projet de prévention et résolution des conflits liés à l’exploitation minière artisanale (PRECEMA) dans la province du Tanganyika - Octobre 2025.", file: "/docs/COTA_Rapport_final_de_mission_de_consultance_Evaluation_finale.pdf" },
-    { title: "Rapport de formation PEAS du Mai 2025", file: "/docs/COTA_Rapport_de_formation_PEAS_Mai_2025.pdf" },
-    { title: "Rapport de consultance : Analyse évaluative des facteurs et dynamiques conflictuelles liés à l’exploitation minière et foncière dans la province du Tanganyika - Décembre 2023", file: "/docs/COTA_Rapport_de_mission_Analyse_évaluative.pdf" },
+    { title: "Rapport de consultance : Evaluation finale externe du projet de prévention et résolution des conflits liés à l’exploitation minière artisanale (PRECEMA) dans la province du Tanganyika - Octobre 2025.", file: "/docs/COTA_Rapport_final_de_mission_de_consultance_Evaluation_finale.pdf"},
+    { title: "Rapport de formation PEAS du Mai 2025", file: "/docs/COTA_Rapport_de_formation_PEAS_Mai_2025.pdf"},
+    { title: "Rapport de consultance : Analyse évaluative des facteurs et dynamiques conflictuelles liés à l’exploitation minière et foncière dans la province du Tanganyika - Décembre 2023", file: "/docs/COTA_Rapport_de_mission_Analyse_évaluative.pdf"},
 
   ],
   autres: [
-    { title: "Formulaire de déclaration du candidat(personne physique)", file: "/docs/COTA_Formulaire_de_déclaration_du_candidat.pdf" },
-    { title: "Formulaire déclarative portant adhésion des nouveaux membres", file: "/docs/COTA_Formulaire_d'adhésion.pdf" },
+    { title: "Formulaire de déclaration du candidat(personne physique)", file: "/docs/COTA_Formulaire_de_déclaration_du_candidat.pdf"},
+    { title: "Formulaire déclarative portant adhésion des nouveaux membres", file: "/docs/COTA_Formulaire_d'adhésion.pdf"},
   ],
 }
